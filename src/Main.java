3f6aab5b4dc3833ede0be23769227f4fe0b5cd52
djaganr <jaganr@apra.in>
2023-01-06 16:39:23 +0530

    this completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         Student student1 = new Student(2, "jagan", 40);
+        student1.object();
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,7 @@ public class Student {
     String name;
 
     Student(int id, String name){
+        // refers to the current class instance
         this.id = id;
         this.name = name;
     }
@@ -10,5 +11,9 @@ public class Student {
     Student(int id, String name, int age){
         this(id, name); // calls the class constructor, reusing constructor
         System.out.println(id + " " + name + " "+ age);
+    }
+
+    void object(){
+        System.out.println(this);
     }
 }
